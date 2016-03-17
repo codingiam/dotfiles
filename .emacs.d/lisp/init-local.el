6a2afb7edf74933b740738fd2deb17cb8755535a
Doru Budai <doru.budai@gmail.com>
2016-03-17 18:43:27 +0200

    Customize ido and add projectile

--- .emacs.d/lisp/init-local.el
+++ .emacs.d/lisp/init-local.el
@@ -1,3 +1,19 @@
+(require-package 'ido-vertical-mode)
+(ido-vertical-mode 1)
 
+(require-package 'flx-ido)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
+(require-package 'rbenv)
+(global-rbenv-mode)
+
+(require-package 'projectile)
+(add-hook 'ruby-mode-hook 'projectile-mode)
+
+;(require-package 'projectile-rails)
+;(add-hook 'projectile-mode-hook 'projectile-rails-on)
 
 (provide 'init-local)
