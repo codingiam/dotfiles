8a7bc21bb1bf321869a0f2d3cf7069e17f93b377
Doru Budai <doru.budai@gmail.com>
2017-07-04 23:17:58 +0300

    Change default font

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,12 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-file-name-transforms (\` ((".*" (\, temporary-file-directory) t))))
  '(backup-directory-alist (\` ((".*" \, temporary-file-directory))))
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes t)
- '(session-use-package t nil (session)))
+ '(desktop-save-mode nil)
+ '(package-selected-packages
+   (quote
+    (evil neotree rbenv flx-ido ido-vertical-mode regex-tool dsvn htmlize lua-mode gnuplot flycheck-ledger ledger-mode origami flycheck-clojure cider elein cljsbuild-mode clojure-mode slime-company hippie-expand-slime slime cask-mode flycheck-package highlight-quoted macrostep cl-lib-highlight aggressive-indent redshank immortal-scratch hl-sexp auto-compile ipretty lively elisp-slime-nav paredit-everywhere paredit sql-indent projectile-rails rinari yaml-mode bundler goto-gem yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax flycheck-elm elm-mode hayoo hindent intero haskell-mode company-anaconda anaconda-mode pip-requirements css-eldoc skewer-less less-css-mode scss-mode sass-mode rainbow-mode tagedit tidy org-pomodoro org-cliplink org-fstree smarty-mode php-mode skewer-mode js-comint xref-js2 coffee-mode js2-mode json-mode erlang csv-nav csv-mode markdown-mode textile-mode crontab-mode alert projectile magit-gh-pulls github-issues github-clone bug-reference-github yagist magit git-timemachine git-messenger gitconfig-mode gitignore-mode git-blame vc-darcs darcsum browse-at-remote fill-column-indicator whitespace-cleanup-mode guide-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy expand-region browse-kill-ring highlight-symbol undo-tree rainbow-delimiters nlinum indent-guide unfill mmm-mode default-text-scale session switch-window company-quickhelp company counsel ivy smex flycheck ibuffer-vc wgrep-ag ag anzu diff-hl dired-sort dired+ disable-mouse color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized mwe-log-commands scratch diminish project-local-variables wgrep exec-path-from-shell fullframe)))
+ '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Meslo LG S" :slant normal :weight normal :height 113 :width normal)))))
